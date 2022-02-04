93a95d5c5841bfc78318ec6821bb51de0b1ec60c
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-04 20:18:40 +1030

    added while looop

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import pandas
 codes = pandas.read_csv("morser_codes.csv")
 
 codes_dict = {row.letter: row.code for (index, row) in codes.iterrows()}
-print(codes_dict)
+
+go_on = True
+while go_on:
+    choice = input("Translate to or from mores code? Enter the related number\n1. English->morse cdode\n"\
+          "2. More code into english\n")
+    if choice == "1":
+        print("English to morse")
+    elif choice == "2":
+        print("Morse to english")
+    else:
+        print("Invalid please try again")
+    break
