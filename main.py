cc79e60b61ebaa7efbf07376aa995b8770f8e72f
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2022-02-04 22:16:03 +1030

    encode morse code working

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ while go_on:
     choice = input("Translate to or from mores code? Enter the related number\n1. English->morse cdode\n"\
           "2. More code into english\n")
     if choice == "1":
-        pass
+        to_decode = input("What do you want to encode\n").upper()
+        array_to_decode = [codes_dict[letter] for letter in to_decode ]
+        print(" ".join(array_to_decode))
+
     elif choice == "2":
         print("Morse to english")
     else:
